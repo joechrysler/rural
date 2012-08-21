a258d0928781887588dae00c4256cbde93f9b137
Joe Chrysler <mail@joechrysler.com>
2012-08-21 18:39:46 -0400

    Init

---
+++ rurler.rb
@@ -0,0 +1,19 @@
+require 'redis'
+require 'sinatra'
+
+set :port, 80
+
+before do
+    @cache = Redis.new
+end
+
+get '/:quicklink' do
+    a = @cache.get params[:quicklink]
+    status 301
+    headers 'Location' => a
+    "I've moved to #{a}"
+end
+
+post '/:quicklink' do
+    @cache.set params[:quicklink], params[:longlink]
+end
